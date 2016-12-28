26fdb65a25accab1a9a971b23365c5e553f2ee5b
paul.xie <paul.xie@chinascope.com>
2016-12-28 17:35:47 +0800

    rename the pattern

--- crawler/market/mwatch.py
+++ crawler/market/mwatch.py
@@ -15,19 +15,21 @@ from pymongo import MongoClient
 
 
 class MarketCrawler(object):
-
     category = "marketwatch"
 
     def __init__(self):
         # db setting
-        self.collection = MongoClient(str(HOST), PORT)[DB][COLLECTION]
+        self.collection = MongoClient(HOST, PORT)[DB][COLLECTION]
 
         # Field
         self.field = ["Name", "Market", "Ticker", ]
 
         # headers
         self.headers = marketwatch_config["headers"]
-        pass
+
+        # Field pattern
+        self.table = '//div[@class="financials"]/table[@class="crDataTable"]'    # 有返回值, 内容存在;
+        self.table_tr = self.table + "//tr/td"
 
     def get_tick_from_db(self):
         coll = self.collection
@@ -45,12 +47,11 @@ class MarketCrawler(object):
         url = marketwatch_config[keyword].format(tick)
         return url
 
-    def download(self):
+    def download(self, url):
         """
         comment by paul.xie
         :return:
         """
-        url = self.get_url()
         try:
             raw_html = requests.get(url, headers=self.headers)
             response = raw_html.text
@@ -58,14 +59,50 @@ class MarketCrawler(object):
         except:
             print ("error")
 
-    def parse(self):
-        seletor = etree.HTML(self.download())
-        
+    def parse_raw_html(self, response):
+        """
+        comment by paul.xie
+        :return:
+        """
+        selector = etree.HTML(response)
+        tr_content = selector.xpath(self.table_tr)
+        print len(tr_content)
+        for one in tr_content:
+            print one
+
+    def unpickle(self, data):
+
         pass
+
+    def ticker_flag(self, url):
+        """
+        comment by paul.xie
+        :return:
+        1. ticker 股票不存在，网页跳转
+        """
+        flag = True
+        raw_html = requests.get(url, headers=self.headers)
+        if url != raw_html.url:
+            flag = False
+        return flag
+
+    def table_flag(self, response):
+        """
+        comment by paul.xie
+        :return:
+        1. ticker 股票存在但没数据
+        """
+        flag = False
+        selector = etree.HTML(response)
+        table_content = selector.xpath(self.table)
+        if table_content:
+            flag = True
+        return flag
 
 
 if __name__ == "__main__":
     marketwatch = MarketCrawler()
     url = marketwatch.get_url("FISV", "INCOME_QUARTER_URL")
     print (url)
-    marketwatch.download()
+    content = marketwatch.download(url)
+    tr_content = marketwatch.parse_raw_html(content)
